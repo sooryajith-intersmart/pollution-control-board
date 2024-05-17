d4f27efa222aa9639f2ecaa07f2690ddff38d746
Sooryajith <sooryajith@intersmart.in>
2024-05-17 11:12:29 +0530

    home

--- Modules/Frontend/routes/web.php
+++ Modules/Frontend/routes/web.php
@@ -16,26 +16,14 @@ use Illuminate\Http\Response;
 |
 */
 
-$allowed_domain = "envirowatch.rotarycochin.com";
-
-if (Request::getHost() === $allowed_domain) {
-    Route::get('/', [FrontendController::class, 'index'])->name('home');
-    Route::get('/get-latest-devices-data', [FrontendController::class, 'getLatestDevicesData'])->name('get-latest-devices-data');
-    Route::fallback(function () {
-        return redirect()->route('home');
-    });
-} else {
-    Route::redirect('/', '/screen-1');
-    Route::get('/screen-1', [FrontendController::class, 'screen1'])->name('screen-1');
-    Route::get('/screen-2', [FrontendController::class, 'screen2'])->name('screen-2');
-    Route::get('/screen-3', [FrontendController::class, 'screen3'])->name('screen-3');
-    Route::get('/screen-4', [FrontendController::class, 'screen4'])->name('screen-4');
-    Route::get('/screen-5', [FrontendController::class, 'screen5'])->name('screen-5');
-    Route::get('/screen-6', [FrontendController::class, 'screen6'])->name('screen-6');
-    Route::get('/get-latest-devices-data', [FrontendController::class, 'getLatestDevicesData'])->name('get-latest-devices-data');
-    Route::fallback(function () {
-        return redirect()->route('screen-1');
-    });
-}
-
-
+Route::get('/', [FrontendController::class, 'index'])->name('home');
+Route::get('/screen-1', [FrontendController::class, 'screen1'])->name('screen-1');
+Route::get('/screen-2', [FrontendController::class, 'screen2'])->name('screen-2');
+Route::get('/screen-3', [FrontendController::class, 'screen3'])->name('screen-3');
+Route::get('/screen-4', [FrontendController::class, 'screen4'])->name('screen-4');
+Route::get('/screen-5', [FrontendController::class, 'screen5'])->name('screen-5');
+Route::get('/screen-6', [FrontendController::class, 'screen6'])->name('screen-6');
+Route::get('/get-latest-devices-data', [FrontendController::class, 'getLatestDevicesData'])->name('get-latest-devices-data');
+Route::fallback(function () {
+    return redirect()->route('home');
+});
