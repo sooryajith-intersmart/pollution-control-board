5676b4c17a1a23a3e7fb8e94b2d0969b675efabe
Sooryajith <sooryajith@intersmart.in>
2024-05-17 11:19:36 +0530

    Enviro Watch : Real-time Pollution Monitoring System

--- Modules/Frontend/resources/views/frontend/index.blade.php
+++ Modules/Frontend/resources/views/frontend/index.blade.php
@@ -1,5 +1,5 @@
 @extends('frontend::layouts.app')
-@section('title', 'AQI')
+@section('title', 'Enviro Watch : Real-time Pollution Monitoring System')
 @section('content')
 <div id="pageWrapper">
     <section class="MainSlide SinglePage">
